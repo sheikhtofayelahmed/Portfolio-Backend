88dcea4ed4a3b4305bbb580a356a76b930b56b66
coding-sta <personal.sta.bd@gmail.com>
2022-06-14 03:31:21 +0600

    single details project

--- index.js
+++ index.js
@@ -9,27 +9,36 @@ const port = process.env.PORT || 5000;
 //MIDDLE WARE
 app.use(cors());
 app.use(express.json());
-const uri = "mongodb+srv://tofayel101:goiUXpPvfREKnU9m@cluster0.urbkh.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri =
+  "mongodb+srv://tofayel101:goiUXpPvfREKnU9m@cluster0.urbkh.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try{
-        client.connect()
-        const projectCollection=client.db("tofayel101").collection("project")
+  try {
+    client.connect();
+    const projectCollection = client.db("tofayel101").collection("project");
 
-        app.get("/projects",async (req,res)=>{
-            const query={}
-            const results=await projectCollection.find(query).toArray()
-            res.send(results)
-        })
-    }
-    finally{
-
-    }
+    app.get("/projects", async (req, res) => {
+      const query = {};
+      const results = await projectCollection.find(query).toArray();
+      res.send(results);
+    });
+    app.get("/projects/:id", async(req, res) => {
+      const id = req.params.id;
+      const query= {_id:ObjectId(id)}
+      const result=await projectCollection.findOne(query)
+      res.send(result)
+    });
+  } finally {
+  }
 }
 run().catch(console.dir);
 
-app.get('/', (req,res)=>{
-    res.send('tofayel101')
-})
-app.listen(port)
+app.get("/", (req, res) => {
+  res.send("tofayel101");
+});
+app.listen(port);
