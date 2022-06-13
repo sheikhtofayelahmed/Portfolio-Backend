3bae5113ca85b26b793595886df5e12b338af6c3
coding-sta <personal.sta.bd@gmail.com>
2022-06-14 03:18:19 +0600

    project done

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -10,3 +9,27 @@ const port = process.env.PORT || 5000;
 //MIDDLE WARE
 app.use(cors());
 app.use(express.json());
+const uri = "mongodb+srv://tofayel101:goiUXpPvfREKnU9m@cluster0.urbkh.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        client.connect()
+        const projectCollection=client.db("tofayel101").collection("project")
+
+        app.get("/projects",async (req,res)=>{
+            const query={}
+            const results=await projectCollection.find(query).toArray()
+            res.send(results)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req,res)=>{
+    res.send('tofayel101')
+})
+app.listen(port)
